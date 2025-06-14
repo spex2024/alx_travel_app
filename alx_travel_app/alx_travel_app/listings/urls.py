f9c104523b2cfd1e95998b0d2d4c53f817ab96fb
spex2024 <dercolbags@gmail.com>
2025-06-14 01:02:50 +0000

    Milestone 3: Creating Views and API Endpoints

--- alx_travel_app/alx_travel_app/listings/urls.py
+++ alx_travel_app/alx_travel_app/listings/urls.py
@@ -1,11 +1,7 @@
+from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import ListingViewSet, BookingViewSet
-
-router = DefaultRouter()
-router.register(r'listings', ListingViewSet)
-router.register(r'bookings', BookingViewSet)
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('', include('listings.urls')),
 ]
