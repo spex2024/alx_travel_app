6b3479e9cd92f8e3f34f8bc190aa2df67f2407d6
spex2024 <dercolbags@gmail.com>
2025-06-14 00:58:25 +0000

    Milestone 3: Creating Views and API Endpoints

--- alx_travel_app/alx_travel_app/listings/models.py
+++ alx_travel_app/alx_travel_app/listings/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Listing(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    location = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Booking(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} -> {self.listing.title}'
--- alx_travel_app/alx_travel_app/listings/urls.py
+++ alx_travel_app/alx_travel_app/listings/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path 
-from . import views 
-urlpatterns = [ 
-    path('', views.index), 
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ListingViewSet, BookingViewSet
+
+router = DefaultRouter()
+router.register(r'listings', ListingViewSet)
+router.register(r'bookings', BookingViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),
 ]
